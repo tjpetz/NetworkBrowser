c9b694631eee73e4590ec24a2871feb1b4e56c57
Thomas Petz, Jr <tjpetz@tjpetz-air.home.tjpetz.com>
2015-12-24 13:46:58 -0500

    Initial Commit

---
+++ Network BrowserTests/Network_BrowserTests.swift
@@ -0,0 +1,36 @@
+//
+//  Network_BrowserTests.swift
+//  Network BrowserTests
+//
+//  Created by Thomas Petz, Jr. on 12/24/15.
+//  Copyright © 2015 Thomas J. Petz, Jr. All rights reserved.
+//
+
+import XCTest
+@testable import Network_Browser
+
+class Network_BrowserTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
