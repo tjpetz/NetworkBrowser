5a1b0cdf74b4438aa235a1950550a801dba68ffa
Thomas Petz, Jr <tjpetz@tjpetz-air.home.tjpetz.com>
2016-09-21 18:27:36 -0400

    Updated file locations.

---
+++ Network Browser/BonjourDomainTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  BonjourDomainTableViewCell.swift
+//  Network Browser
+//
+//  Created by Thomas Petz, Jr. on 12/29/15.
+//  Copyright © 2015 Thomas J. Petz, Jr. All rights reserved.
+//
+
+import UIKit
+
+class BonjourDomainTableViewCell: UITableViewCell {
+
+    // MARK: Properties
+    @IBOutlet weak var domain: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
